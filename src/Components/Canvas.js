c32233bf3a7fa616e2b0209db3242d24f0b7fa99
David Zarate <davidmanolo_97@hotmail.com>
2021-08-21 11:06:38 -0600

    Bola

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,5 +1,4 @@
-import React,{useState} from 'react';
-
+import React, { useRef, useEffect ,useState} from 'react'
 /*
 * 
 * Description:
@@ -17,7 +16,47 @@ const Canvas = (props) => {
 
     const run = () =>  alert("To be implemented")
     const [inputWord,setInputWord]=useState('');
+    const canvasRef = useRef(null)
+    const [context,setCanvasObj]=useState(null);
+    const [mouseRelativeToNode,setMouseRelativeToNode]=useState({});
+    const algo=(e)=>{
+        //Our first draw
+        context.clearRect(0, 0, 600, 600);
+        context.globalAlpha = 0.2;
+        context.fillStyle = "white";
+        context.fillRect(0, 0, 600, 600);
+        context.globalAlpha = 1;
+        
+      
+        context.beginPath();
+
+        context.arc(e.nativeEvent.offsetX,
+            e.nativeEvent.offsetY,
+            20,
+            0,
+            Math.PI * 2,
+            false);
+            context.globalAlpha = 1;
+            context.fillStyle = "rgb(211, 195, 128)";
+            context.fill();
+            context.globalAlpha = 1;
+            context.strokeStyle = "rgb(145, 127, 49)";
+            context.stroke();
+            context.globalAlpha = 1;
+            context.font = "15px Georgia";
+            context.textAlign = "center";
+            context.textBaseline = "middle";
+            context.fillStyle = "rgb(6, 11, 16)";
+            context.fillText('0',
+            e.nativeEvent.offsetX,
+            e.nativeEvent.offsetY);
+           
+        }
+    useEffect(() => {
+     
+      setCanvasObj(canvasRef.current.getContext('2d'));
     
+    }, [])
     
     return (             
         <div id = "c2">
@@ -29,8 +68,8 @@ const Canvas = (props) => {
                     <input type="text" onChange={(e)=>setInputWord(e.target.value)} value={inputWord} id="input-word"/> <input id="run-button" type="submit" disabled={inputWord!==''?false:true} className="button" value="Run" onClick={run} /><br></br><br></br>
                     </React.Fragment>
                 }
-          
-                <canvas style={{background:props.showRunBotton?"#f7f1e3":""}} id="main-canvas" width="600" height="600"></canvas>
+            
+                <canvas onClick={(e)=>algo(e)} ref={canvasRef}  style={{background:props.showRunBotton?"#f7f1e3":""}} id="main-canvas" width="600" height="600"/>
             </div>
         </div>
      );
