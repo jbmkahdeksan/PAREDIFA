0491236d6739abd76cacd4dd562b81ddc206a278
David Zarate <68641019+No1s-Perfect@users.noreply.github.com>
2021-11-13 13:25:00 -0600

    .

--- Server/app.js
+++ Server/app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, "..", "build")));
-app.use(express.static("public"));
+
 
 app.post("/gql", graphqlHTTP({ schema: schema, graphiql: true }));
 
