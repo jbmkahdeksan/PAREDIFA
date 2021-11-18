3593b996cf11a32d6d3b225685a80c010e5ff216
David Zarate <68641019+No1s-Perfect@users.noreply.github.com>
2021-11-17 19:46:59 -0600

    m changes

--- src/Components/LayingDFA/AnimationLayingDfa.js
+++ src/Components/LayingDFA/AnimationLayingDfa.js
@@ -3,8 +3,8 @@ import { FcMindMap } from "react-icons/fc";
 const AnimationLayingDfa = () => {
   return (
     <div className="layingDFA">
-      <FcMindMap size={40} id="hammer" className="" />
-      <BsHammer size={40} id="gear1" className="fa fa-5x fa-gear spin" />
+      <FcMindMap size={40} id="dfaIcon" />
+      <BsHammer size={40}  className="spin" />
       <h4>Laying out DFA, please wait...</h4>
     </div>
   );
