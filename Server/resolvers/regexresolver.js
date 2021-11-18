2ed51228f9ae1415b6982d9e46597a189bcddcae
David Zarate <68641019+No1s-Perfect@users.noreply.github.com>
2021-11-18 00:33:20 -0600

    divide and conquer

--- Server/resolvers/regexresolver.js
+++ Server/resolvers/regexresolver.js
@@ -21,16 +21,19 @@ async function compileRE(re) {
   const prologEndPoint = re.simpBeforeComp ? "http://localhost:9000/simplifier" 
                                            : "http://localhost:9000/compiler";
   const regularExpression = { value: re.RE, type: "regex" };//Object that is to be sent
-  let response = await axios.post(prologEndPoint, regularExpression);
-  let finiteAutomata = response.data.fa;
-  return {
-    nodes: finiteAutomata.states.map((state, index) => ({
+  try{
+    let response = await axios.post(prologEndPoint, regularExpression);
+    let finiteAutomata = response.data.fa;
+  
+    const processedNodes = finiteAutomata.states.map((state, index) => ({
       name: index,
       label: `S${index}`,
       initial: finiteAutomata.initial === state,
       final: finiteAutomata.finals.some((final) => final === state),
-    })),
-    edges: finiteAutomata.moves.map((move) => {
+    }));
+  
+    //Procesa los movimientos que vienen del servidor
+    let edges = finiteAutomata.moves.map((move) => {
       let parsedMove =  move.split('==>').flatMap(i => i.split("/"));
       const movementSource = finiteAutomata.states.indexOf(parsedMove[0]);
       const movementTarget = finiteAutomata.states.indexOf(parsedMove[2]);
@@ -39,9 +42,31 @@ async function compileRE(re) {
         target: movementTarget,
         symbol: parsedMove[1],
       })
-  }),
-    alphabet: finiteAutomata.vocabulary,
-  };
+    })
+  
+    //Almacena los movimientos procesados para no volver a hacerlo
+    let visitedEdges = [];
+    
+    //Unifica los simbolos de los movimientos de un mismo source y target
+    const processedEdges = edges.reduce((acc, currentEdge) => {
+      if(!visitedEdges.some(coord => coord.source === currentEdge.source && coord.target === currentEdge.target)){
+        let unifiedSymbols = (edges.filter(e => e.source === currentEdge.source && e.target === currentEdge.target)
+                             .map(edge => edge.symbol)).join();
+        currentEdge.symbol = unifiedSymbols;
+        visitedEdges.push( ({source:currentEdge.source, target:currentEdge.target}) );
+        return [...acc,currentEdge]
+      }
+      return [...acc];
+    }, [])
+    return {
+      nodes: processedNodes,
+      edges: processedEdges,
+      alphabet: finiteAutomata.vocabulary,
+    };
+
+  }catch(e){
+    return e
+  }
 }
 module.exports = {
   compileRE,
